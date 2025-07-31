caab2662189f22b2443f1bd4b280869badad224d
Felicity Chun <felicity.chun@anu.edu.au>
2025-07-31 13:46:52 +1000

    feedback dive down 4, feedback folder

--- recipe_diagnostics/diagnostic_scripts/feedback/feedback_nhf_lvl3_4.py
+++ recipe_diagnostics/diagnostic_scripts/feedback/feedback_nhf_lvl3_4.py
@@ -1,4 +1,4 @@
-"""diagnostic script to plot ENSO feedback metrics"""
+"""diagnostic script to plot ENSO sst nhf feedback metrics level 3 and 4"""
 
 import os
 import logging
@@ -18,15 +18,34 @@ from esmvalcore.preprocessor import (mask_above_threshold,
                                      mask_below_threshold,
                                      rolling_window_statistics,
                                      convert_units,
+                                     extract_month,
+                                     climate_statistics,
                                      )
 
 
 # This part sends debug statements to stdout
 logger = logging.getLogger(os.path.basename(__file__))
 
+    
+def annual_structure_reg(nhf_cube, ts_cube):
+    months_reg = []
+    for i in range(1,13):
+        #extract for both cubes
+        nhf = extract_month(nhf_cube, month=i)
+        ts = extract_month(ts_cube, month=i)
+        coefs = lin_regress_matrix(nhf, ts, level4=True)
+        # collect array for months
+        months_reg.append(coefs)
 
+    # get month_number coordinate from an existing cube
+    m_cube = climate_statistics(ts_cube, operator='max', period='month')
+    
+    #create cube, x = months, y = lon
+    result_cube = iris.cube.Cube(np.array(months_reg), long_name='regression',
+                                 dim_coords_and_dims=[(nhf.coord('longitude'), 1), (m_cube.coord('month_number'), 0)])
+    return result_cube
 
-def lin_regress_matrix(cubeA, cubeBsst):
+def lin_regress_matrix(cubeA, cubeBsst, level4=False):
     
     A_data = cubeA.data.reshape(cubeA.shape[0], -1)  # Shape (time, spatial_points)
     if cubeA.shape[0] == cubeBsst.shape[0]:
@@ -41,14 +60,17 @@ def lin_regress_matrix(cubeA, cubeBsst):
     # Solve the linear equations using least squares method
     coefs, _, _, _ = np.linalg.lstsq(B_with_intercept, A_data, rcond=None)
 
-    # Create a new Iris Cube for the regression results
-    result_cube = iris.cube.Cube(coefs[0], long_name='regression A',
-                                 dim_coords_and_dims=[(cubeA.coord('longitude'), 0)])
+    if level4:
+        return coefs[0] 
+    else:
+        # Create a new Iris Cube for the regression results
+        result_cube = iris.cube.Cube(coefs[0], long_name='regression A',
+                                    dim_coords_and_dims=[(cubeA.coord('longitude'), 0)])
 
-    return result_cube
+        return result_cube
 
 
-def feedback_nonlin(sst_cube, tauu_cube):
+def feedback_nonlin(sst_cube, tauu_cube, level4=False):
     tauu_aux = tauu_cube.copy()
     sst_coord = iris.coords.AuxCoord(sst_cube.data, sst_cube.standard_name, sst_cube.long_name, sst_cube.var_name, sst_cube.units)
     tauu_aux.add_aux_coord(sst_coord, 0)
@@ -61,16 +83,21 @@ def feedback_nonlin(sst_cube, tauu_cube):
     xbelow0 = tauu_aux.extract(below0)
     xabove0 = tauu_aux.extract(above0)
 
-    ky, cnts = np.unique(ssta_pos.data.mask, return_counts=True)
-    msk_pos = dict(zip(ky.tolist(), cnts.tolist()))
-    ky, cnts = np.unique(ssta_neg.data.mask, return_counts=True)
-    msk_neg = dict(zip(ky.tolist(), cnts.tolist()))
-    msk_dt = {'pos': msk_pos[False], 'neg':msk_neg[False]}
-    logger.info(f'pos: {msk_pos[False]} cube: {xabove0.shape}, neg: {msk_neg[False]} cube: {xbelow0.shape}')
-    outreg_cube = lin_regress_matrix(xbelow0, ssta_neg)
-    posreg_cube = lin_regress_matrix(xabove0, ssta_pos)
+    if level4:
+        outreg_cube = annual_structure_reg(xbelow0, ssta_neg)
+        posreg_cube = annual_structure_reg(xabove0, ssta_pos)
+        all_cube = annual_structure_reg(tauu_cube, sst_cube)
+    else:
+        ky, cnts = np.unique(ssta_pos.data.mask, return_counts=True)
+        msk_pos = dict(zip(ky.tolist(), cnts.tolist()))
+        ky, cnts = np.unique(ssta_neg.data.mask, return_counts=True)
+        msk_neg = dict(zip(ky.tolist(), cnts.tolist()))
+        all_cube = {'pos': msk_pos[False], 'neg':msk_neg[False]}
+        logger.info(f'pos: {msk_pos[False]} cube: {xabove0.shape}, neg: {msk_neg[False]} cube: {xbelow0.shape}')
+        outreg_cube = lin_regress_matrix(xbelow0, ssta_neg)
+        posreg_cube = lin_regress_matrix(xabove0, ssta_pos)
 
-    return outreg_cube, posreg_cube, msk_dt
+    return all_cube, outreg_cube, posreg_cube
 
 def obs_extract_overlap(obs_1, obs_2):
     """Extract overlapping time range from two observation datasets."""
@@ -99,21 +126,17 @@ def format_longitude(x, pos):
 def plot_level3(obs_ds, model_ds, metric_varls, ds_labels, title): #edit for nhf
     """Plot level 3 diagnostics for ENSO feedback metrics."""
     figure = plt.figure(figsize=(10, 6), dpi=300)
-    tau_modcube = rolling_window_statistics(model_ds[metric_varls[1]], 
-                                     coordinate='longitude',operator='mean',window_length=30)
-    tau_obcube = rolling_window_statistics(obs_ds[metric_varls[2]], # check index
-                                     coordinate='longitude',operator='mean',window_length=30)
     # plot whole regression
-    cb = lin_regress_matrix(tau_modcube, model_ds[metric_varls[0]])            
+    cb = lin_regress_matrix(model_ds[metric_varls[1]], model_ds[metric_varls[0]])            
     qplt.plot(cb, color='black', linestyle='solid', label=ds_labels[1])
 
     # obs datasets can have different time range..
-    obs1, obs2 = obs_extract_overlap(tau_obcube, obs_ds[metric_varls[0]])
+    obs1, obs2 = obs_extract_overlap(obs_ds[metric_varls[2]], obs_ds[metric_varls[0]])
     # obs_ds_label = f"{input_data['obs'][0]['dataset']}_{input_data['obs'][1]['dataset']}"
     cb2 = lin_regress_matrix(obs1, obs2)
     qplt.plot(cb2, color='black', linestyle='--', label=ds_labels[0])
     # process model data split
-    neg, pos, cnts = feedback_nonlin(model_ds[metric_varls[0]], tau_modcube)
+    cnts, neg, pos = feedback_nonlin(model_ds[metric_varls[0]], model_ds[metric_varls[1]])
     
     xvar = metric_varls[0].split('_')[0] # ts, tauu, ssh
     yvar = metric_varls[2].split('_')[0]
@@ -121,7 +144,7 @@ def plot_level3(obs_ds, model_ds, metric_varls, ds_labels, title): #edit for nhf
     qplt.plot(neg, color='blue', linestyle='solid', label=f"{xvar.upper()}A<0")
     qplt.plot(pos, color='red', linestyle='solid', label=f"{xvar.upper()}A>0")
     # process obs data split
-    neg, pos, cnts = feedback_nonlin(obs2, obs1)
+    cnts, neg, pos = feedback_nonlin(obs2, obs1)
     qplt.plot(neg, color='blue', linestyle='--')
     qplt.plot(pos, color='red', linestyle='--')
 
@@ -132,6 +155,57 @@ def plot_level3(obs_ds, model_ds, metric_varls, ds_labels, title): #edit for nhf
     plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(format_longitude))
     plt.legend()
     plt.title(title)
+    return figure
+
+def plot_level_4(obs_ds, model_ds, metric_varls, ds_labels):
+    """Plot level 4 diagnostics for ENSO feedback metrics."""
+    obs1, obs2 = obs_extract_overlap(obs_ds[metric_varls[2]], obs_ds[metric_varls[0]])
+
+    xvar = metric_varls[0].split('_')[0] # ts, tauu, ssh
+    yvar = metric_varls[2].split('_')[0]
+    
+    modells = feedback_nonlin(model_ds[metric_varls[0]], model_ds[metric_varls[1]], level4=True)
+    obsls = feedback_nonlin(obs2, obs1, level4=True)
+    sub = ['', '>0', '<0',] #all_cube, posreg_cube, outreg_cube
+
+    figure = plt.figure(figsize=(8, 12), dpi=300)
+    i = 321
+    ax1 = plt.subplot(i)
+
+    for isub, cbls in enumerate(zip(obsls, modells)):
+        for cb in cbls:
+            if i == 321:
+                ax1.set_yticks(range(1, 13, 4))
+                ax1.set_yticklabels(['Jan', 'May', 'Sep'] )
+                ax1.set_ylabel('Months')
+                ax1.set_title(f'ref: {ds_labels[0]} \nreg({xvar.upper()}A, {yvar.upper()}A)', loc='left') #label
+            else:
+                ax2 = plt.subplot(i, sharex=ax1, sharey=ax1)
+                ax2.set_title(f'reg({xvar.upper()}A{sub[isub]}, {yvar.upper()}A)', loc='left')
+                if i == 322:
+                    ax2.set_title(f'{ds_labels[1]} \nreg({xvar.upper()}A, {yvar.upper()}A)', loc='left')
+
+            ## contour plt data
+            cf1 = iplt.contourf(cb, coords=['longitude','month_number'], levels=np.arange(-50, 50, 5), extend='both', cmap="RdBu_r")
+            
+            if i < 325:
+                plt.tick_params('x', labelbottom=False)
+            else:
+                plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(format_longitude)) #apply to bottom left =>325
+                ax2.set_xlabel("longitude")
+            
+            if i%2 == 0:
+                plt.tick_params(labelleft=False)
+            else:
+                ax2.set_ylabel('Months')
+            
+            i += 1
+
+    plt.subplots_adjust(top=0.95)
+    cax = plt.axes([0.15,0.04,0.7,0.02])
+    cbar = fig.colorbar(cf1, cax=cax, orientation='horizontal', extend='both', ticks=np.arange(-50, 51, 25))
+    cbar.set_label(f'regression ((W/m2/°C)') #
+
     return figure
 
 def get_provenance_record(caption, ancestor_files):
@@ -154,6 +228,13 @@ def get_provenance_record(caption, ancestor_files):
     }
     return record
 
+def _fix_time_coord(cube, _, _filename): #fix obs data time differs
+    """Set time points to same day and hour for monthly data."""
+    t_coord = cube.coord("time")
+    _unit = t_coord.units
+    new_time = [d.replace(day=15, hour=0) for d in _unit.num2date(t_coord.points)]
+    t_coord.points = _unit.date2num(new_time).astype("float64")
+
 def main(cfg):
     """Run ENSO feedback metrics."""
 
@@ -173,8 +254,11 @@ def main(cfg):
     logger.info(msg)
 
     # group models by dataset
-    model_ds = group_metadata(models, 'dataset', sort='project')        
-    
+    model_ds = group_metadata(models, 'dataset', sort='project')
+
+    obs_ds = {dataset['variable_group']: iris.load_cube(dataset['filename'], callback=_fix_time_coord) for dataset in obs}
+    obs_ds['nhf_eqp_obs'] = rolling_window_statistics(obs_ds['nhf_eqp_obs'], # check index
+                                     coordinate='longitude',operator='mean',window_length=30)
     # dataset name
     for dataset, mod_ds in model_ds.items():
         logger.info(f"{metric}, preprocessed cubes:{len(model_ds)}, dataset:{dataset}")
@@ -182,18 +266,22 @@ def main(cfg):
                     for ds in obs] + [ds['filename'] 
                                         for ds in model_ds[dataset]]
 
-        
-        obs_ds = {dataset['variable_group']: iris.load_cube(dataset['filename']) for dataset in obs}
         model = {attributes['variable_group']: iris.load_cube(attributes['filename']) 
                                 for attributes in mod_ds}
         model['nhf_eqp_mod'] = - model['nhf_eqp_mod'] # make negative
-        
+        model['nhf_eqp_mod'] = rolling_window_statistics(model['nhf_eqp_mod'], 
+                                     coordinate='longitude',operator='mean',window_length=30)
+
         ds_labels = [f"{obs[0]['dataset']}_{obs[1]['dataset']}", mod_ds[0]['dataset']]
         title = f"net heat flux feedback"
         # plot level 3
-        prov_record = get_provenance_record(f'ENSO metrics {metric} feedback level3', dt_files)
+        prov_record = get_provenance_record(f'ENSO metrics {metric} feedback level 3', dt_files)
         fig = plot_level3(obs_ds, model, SST_NHF, ds_labels, title)
-        save_figure(f'{dataset}_{metric}_lvl3', prov_record, cfg, figure=fig, dpi=300)#
+        save_figure(f'{dataset}_{metric}_lvl3', prov_record, cfg, figure=fig, dpi=300)
+        
+        prov_record = get_provenance_record(f'ENSO metrics {metric} feedback level 4', dt_files)
+        fig = plot_level_4(obs_ds, model, SST_NHF, ds_labels)
+        save_figure(f'{dataset}_{metric}_lvl4', prov_record, cfg, figure=fig, dpi=300)#
 
 
 if __name__ == '__main__':

ead9bee473d5cb9f8df53a614cb03993cbe69506
rhaegar325 <rhaegar227@gmail.com>
2025-08-25 10:32:13 +1000

    update regional parameter

--- visualization_tools/visualization.py
+++ visualization_tools/visualization.py
@@ -50,7 +50,7 @@ class Visualization():
     """
 
     
-    def __init__(self, data, size, lat_range, lon_range, title, colormap, tickvalues, vmax, vmin, resolution=(700, 700)):
+    def __init__(self, data, size, lat_range, lon_range, title, colormap, tickvalues, vmax, vmin, resolution=(700, 700), region=None):
         self.data = data
         self.size = size
         self.width = size[0]
@@ -63,6 +63,8 @@ class Visualization():
         self.resolution =  resolution
         self.vmax = vmax
         self.vmin = vmin
+        self.region = region
+        
 
     def generate_rgba(self):
         data = self.data.data  # 2D numpy array
@@ -88,6 +90,7 @@ class Visualization():
         
         return rgba
 
+
     def resize_rgba(self, rgba, width, height):
         #If the original image is of type uint8, it needs to be converted to float32 before resizing.
         rgba_float = rgba.astype(np.float32) / 255.0
@@ -99,6 +102,7 @@ class Visualization():
         rgba_resized = np.clip(rgba_resized * 255, 0, 255).astype(np.uint8)
     
         return rgba_resized
+
 
     def normalise_array(self, values, minimum=None, maximum=None):
         """
@@ -126,6 +130,7 @@ class Visualization():
         array = np.clip(array, 0, 1)
     
         return array
+
         
     def pad_rgba(self, data, pad_width, pad_height, pad_depth=None, constant_values=255):
         if pad_depth:
@@ -145,18 +150,129 @@ class Visualization():
             
         return padded_rgba
 
-    # def opacity_rgba(self, padded_array, opacity_array):
-    #     array = self.normalise_array(opacity_array)
-    #     oarray = array
-    #     oarray = np.nan_to_num(oarray)
-    #     oarray = (oarray * 255).astype(np.uint8)
-    #     padded_array[::, ::, 3] = oarray
-        
-    #     return padded_array
+
+    def rotate_region(self, lv):
+        cam_africa={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [0.015515, -0.213022, 0.000855, 0.976923],
+            'xyzrotate': [1.887586, -24.597353, -0.311234],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_euro={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [0.355491, -0.340382, -0.278646, 0.824683],
+            'xyzrotate': [56.403061, -21.303127, -48.855858],
+            'fov': 45.0,
+             'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_aisa={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [0.102419, -0.760827, -0.207076, 0.606436],
+            'xyzrotate': [112.134262, -61.686878, -120.888702],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_aust={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [0.003478, -0.91839, 0.218037, 0.330141],
+            'xyzrotate': [-149.899796, -37.438725, 170.021637],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_north_am={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [-0.165649, -0.746298, -0.326733, -0.5557],
+            'xyzrotate': [104.104782, 46.152786, 118.210793],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_south_am={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [0.130201, -0.517479, 0.102816, -0.839431],
+            'xyzrotate': [-37.048805, 57.352139, -34.736542],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_anta={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [-0.207506, -0.678652, 0.653672, 0.262741],
+            'xyzrotate': [-90.41713, -4.895439, 141.134872],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_arct={
+            'translate': [0.0, 0.0, -22.032022],
+            'rotate': [0.248796, -0.694516, -0.63765, 0.221573],
+            'xyzrotate': [95.078087, 0.545349, -141.07901],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_paci={
+            'translate': [0.0, 0.0, -22.032022],
+            'rotate': [0.036096, -0.990832, 0.024548, -0.127577],
+            'xyzrotate': [-176.572876, 14.539167, -175.389648],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_indi={
+            'translate': [0.0, 0.0, -22.032022],
+            'rotate': [-0.102844, -0.613056, 0.101087, 0.77672],
+            'xyzrotate': [-51.315109, -68.678268, 51.170242],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        cam_atla={
+            'translate': [0.0, 0.0, -22.071718],
+            'rotate': [-0.004712, 0.202786, -0.002818, 0.979204],
+            'xyzrotate': [-0.647486, 23.397743, -0.463794],
+            'fov': 45.0,
+            'focus': [-0.014273, -0.037801, 0.001495]
+        }
+
+        match self.region:
+            case 'Africa'|'afri':
+                lv.camera(cam_africa)
+            case 'Europe'|'euro':
+                lv.camera(cam_euro)
+            case 'Aisa'|'aisa':
+                lv.camera(cam_aisa)
+            case 'Australia'|'aust':
+                lv.camera(cam_aust)
+            case 'North_America'|'noam':
+                lv.camera(cam_north_am)
+            case 'South_America'|'soam':
+                lv.camera(cam_south_am)
+            case 'Antarctica'|'anta':
+                lv.camera(cam_anta)
+            case 'Arctic'|'arct':
+                lv.camera(cam_arct)
+            case 'Pacific'|'paci':
+                lv.camera(cam_paci)
+            case 'Atlantic'|'atla':
+                lv.camera(cam_atla)
+            case 'Indian'|'indi':
+                lv.camera(cam_indi)
+
+        return lv
+                            
 
     def generate_lv(self):
         lv = accessvis.plot_earth(texture='bluemarble', background="white", vertical_exaggeration=20)
-        lv.rotation(15.0, -200.0, 0.0) #Rotate to Pacific
+        if self.region is None:
+            lv.rotation(15.0, -200.0, 0.0) #Rotate to Pacific
+        else:
+            lv=self.rotate_region(lv)
         lv.set_properties(diffuse=0.8, ambient=0.1, specular=0.35, shininess=0.03, light=[1,1,0.98]) # make pretty
         lv.brightness_contrast_saturation(0.5, 0.5, 0.65)
 
@@ -166,26 +282,11 @@ class Visualization():
         
         return lv
 
+
     def calculate_pad(self):
         self.pad_width = ((self.lat_range[0]-(-90))*5, (90-self.lat_range[1])*5)
         self.pad_height = (self.lon_range[0]*5, (360 - self.lon_range[1])*5)
 
-    # def visualise_contourf(self, window=False):
-    #     self.calculate_pad()
-    #     rgba=self.generate_rgba()
-    #     resized_rgba=self.resize_rgba(rgba,width=self.width,height=self.height)
-    #     padded_rgba=self.pad_rgba(resized_rgba,pad_width=self.pad_width,pad_height=self.pad_height,pad_depth=(0, 0))
-    #     opacity_array = resize(self.data.data, self.size, order=1, preserve_range=True, anti_aliasing=True)
-    #     padded_opacity_array=self.pad_rgba(opacity_array,pad_width=self.pad_width,pad_height=self.pad_height,constant_values=0)
-    #     opacitied_rgba=self.opacity_rgba(padded_rgba, padded_opacity_array)
-
-    #     lv = self.generate_lv()
-    #     accessvis.update_earth_values(lv, dataMode=0, data=opacitied_rgba)
-
-    #     if window:
-    #         lv.window(resolution=self.resolution)
-    #     else:
-    #         lv.display(resolution=self.resolution)
 
     def visualise_contourf(self, window=False, Alpha=0.7):
         self.calculate_pad()
